0234254fe964750ec938e8ecc4e403caa0f6d917
Khoa100 <minhkhoavu954@gmail.com>
2018-09-06 11:46:29 -0700

    Add files via upload

---
+++ Dotify/Dotify/Dotify/Song.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dotify
+{
+    class Song
+    {
+        string title;
+        string artist;
+        string album;
+        byte[] picture;
+        byte[] music;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Artist
+        {
+            get { return artist; }
+            set { artist = value; }
+        }
+
+        public string Album
+        {
+            get { return album; }
+            set { album = value; }
+        }
+        public byte[] Picture
+        {
+            get { return picture; }
+            set { picture = value; }
+        }
+        public byte[] Music
+        {
+            get { return music; }
+            set { music = value; }
+        }
+    }
+}
