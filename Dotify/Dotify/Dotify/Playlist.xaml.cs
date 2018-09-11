563cd110d0b3430c737828174399f052828273a7
Khoa100 <minhkhoavu954@gmail.com>
2018-09-11 13:02:01 -0700

    Add files via upload

--- Dotify/Dotify/Dotify/Playlist.xaml.cs
+++ Dotify/Dotify/Dotify/Playlist.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,26 @@ namespace Dotify
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Playlist : ContentPage
 	{
-		public Playlist ()
+        private static ObservableCollection<PlaylistInfo> playlists = new ObservableCollection<PlaylistInfo> {
+            new PlaylistInfo("Workout"),
+            new PlaylistInfo("Studying"),
+            new PlaylistInfo("Travel")
+        };
+        
+        public Playlist ()
 		{
 			InitializeComponent ();
-		}
-	}
+            PlaylistView.ItemsSource = playlists;
+        }
+        public static ObservableCollection<PlaylistInfo> PlaylistSet
+        {
+            get { return playlists; }
+            set { playlists = value; }
+        }
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {            
+            var newPage = new UserInput();
+            await Navigation.PushModalAsync(newPage);
+        }
+    }
 }
---
+++ Dotify/Dotify/Dotify/PlaylistInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Dotify
+{
+    public class PlaylistInfo
+    {
+        string listName;
+        List<Song> songs;
+
+        public PlaylistInfo(string name)
+        {
+            this.ListName = name;
+            songs = new List<Song>();
+        }
+        public string ListName
+        {
+            get { return listName; }
+            set { listName = value; }
+        }
+       
+    }
+}
