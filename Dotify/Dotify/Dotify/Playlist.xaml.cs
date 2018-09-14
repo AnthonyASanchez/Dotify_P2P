d303d7a3ca4521ee66eb7a40dfce366f4d0ee1b1
Khoa100 <minhkhoavu954@gmail.com>
2018-09-14 15:48:24 -0700

    Add files via upload

--- Dotify/Dotify/Dotify/Playlist.xaml.cs
+++ Dotify/Dotify/Dotify/Playlist.xaml.cs
@@ -1,23 +1,24 @@
-﻿using System;
+﻿
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace Dotify
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Playlist : ContentPage
 	{
-        private static ObservableCollection<PlaylistInfo> playlists = new ObservableCollection<PlaylistInfo> {
-            new PlaylistInfo("Workout"),
-            new PlaylistInfo("Studying"),
-            new PlaylistInfo("Travel")
-        };
+        private static ObservableCollection<PlaylistInfo> playlists = new ObservableCollection<PlaylistInfo>();
         
         public Playlist ()
 		{
@@ -29,10 +30,10 @@ namespace Dotify
             get { return playlists; }
             set { playlists = value; }
         }
-        private async void OnButtonClicked(object sender, EventArgs e)
-        {            
-            var newPage = new UserInput();
-            await Navigation.PushModalAsync(newPage);
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            var page = new Popup();
+            Navigation.PushModalAsync(page);
         }
     }
 }
---
+++ Dotify/Dotify/Dotify/Popup.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static Java.Text.Normalizer;
+
+namespace Dotify
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Popup : PopupPage
+	{
+		public Popup ()
+		{
+			InitializeComponent ();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            string input = PlaylistName.Text;
+            PlaylistInfo playlist = new PlaylistInfo(input);
+            bool newlistExists = Playlist.PlaylistSet.Contains(playlist);
+            if(newlistExists == true)
+            {
+                await DisplayAlert("Create Failed:", input + " already exists.", "Ok");
+            }
+            else
+            {
+                Playlist.PlaylistSet.Add(playlist);
+                await DisplayAlert("Confirmation", input + " has been added.", "Ok");
+            }
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            string input = PlaylistName.Text;
+            PlaylistInfo playlist = new PlaylistInfo(input);
+            bool removed = Playlist.PlaylistSet.Remove(playlist);
+            if(removed == true)
+            {
+                await DisplayAlert("Confirmation", input + " has been removed.", "Cancel");
+            }
+            else
+            {
+                await DisplayAlert("Could Not Remove", input + " could not be found.", "Cancel");
+            }
+                 
+        }
+
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            var main = new MainPage();
+            await Navigation.PushModalAsync(main);
+        }
+    }
+}
